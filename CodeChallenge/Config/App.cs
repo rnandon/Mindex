049f702a66c7da7d0ebbbf515de4bd088c063ee7
Ryan Nance-Donaldson <70601939+rnandon@users.noreply.github.com>
2024-06-07 15:38:21 -0500

    Updated app configuration to include new Compensation service and repository

--- CodeChallenge/Config/App.cs
+++ CodeChallenge/Config/App.cs
@@ -44,6 +44,8 @@ namespace CodeChallenge.Config
 
             services.AddScoped<IEmployeeService, EmployeeService>();
             services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<ICompensationService, CompensationService>();
+            services.AddScoped<ICompensationRepository, CompensationRepository>();
 
             services.AddControllers();
         }
