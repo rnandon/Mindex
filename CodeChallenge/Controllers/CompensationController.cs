10f11649cde2118df9fbee46673055b65ba30362
Ryan Nance-Donaldson <70601939+rnandon@users.noreply.github.com>
2024-06-07 15:38:47 -0500

    Added CompensationController with create and get endpoints

---
+++ CodeChallenge/Controllers/CompensationController.cs
@@ -0,0 +1,48 @@
+﻿using CodeChallenge.Models;
+using CodeChallenge.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace CodeChallenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompensationController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly ICompensationService _compensationService;
+        private readonly IEmployeeService _employeeService;
+        
+        public CompensationController(ILogger<CompensationController> logger, ICompensationService compensationService, IEmployeeService employeeService)
+        {
+            _logger = logger;
+            _compensationService = compensationService;
+            _employeeService = employeeService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompensation([FromBody] Compensation compensation)
+        {
+            _logger.LogDebug($"Received compensation create request for '{compensation.EmployeeId}'");
+
+            _compensationService.Create(compensation);
+
+            return CreatedAtRoute("getCompensationById", new { id = compensation.EmployeeId }, compensation);
+        }
+
+        [HttpGet("{id}", Name = "getCompensationById")]
+        public IActionResult GetCompensationById(String id)
+        {
+            _logger.LogDebug($"Received compensation get request for '{id}'");
+
+            var compensation = _compensationService.GetById(id);
+
+            if (compensation == null)
+                return NotFound();
+
+            return Ok(compensation);
+        }
+    }
+}
