6d5f45759d68005abb34cad0bd7a17639fb53999
Ryan Nance-Donaldson <70601939+rnandon@users.noreply.github.com>
2024-06-07 15:36:45 -0500

    Added CompensationRepository and associated interface

---
+++ CodeChallenge/Repositories/CompensationRepository.cs
@@ -0,0 +1,36 @@
+﻿using CodeChallenge.Data;
+using CodeChallenge.Models;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.Repositories
+{
+    public class CompensationRepository : ICompensationRepository
+    {
+        private readonly ILogger<ICompensationRepository> _logger;
+        private readonly EmployeeContext _employeeContext;
+
+        public CompensationRepository(ILogger<ICompensationRepository> logger, EmployeeContext employeeContext)
+        {
+            _logger = logger;
+            _employeeContext = employeeContext;
+        }
+
+        public Compensation Add(Compensation compensation)
+        {
+            _employeeContext.Compensations.Add(compensation);
+            return compensation;
+        }
+
+        public Compensation GetById(string id)
+        {
+            return _employeeContext.Compensations.SingleOrDefault(c => c.EmployeeId == id);
+        }
+
+        public Task SaveAsync()
+        {
+            return _employeeContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ CodeChallenge/Repositories/ICompensationRepository.cs
@@ -0,0 +1,12 @@
+﻿using CodeChallenge.Models;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.Repositories
+{
+    public interface ICompensationRepository
+    {
+        public Compensation Add(Compensation compensation);
+        public Compensation GetById(string id);
+        public Task SaveAsync();
+    }
+}
